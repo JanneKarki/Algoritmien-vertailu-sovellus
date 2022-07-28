16ef093cca07ffedc85d460e606414acfcb3ca0d
Janne Kärki <janne.karki@helsinki.fi>
2022-07-28 19:38:59 +0300

    alustava dict verkon solmuille

--- src/maze.py
+++ src/maze.py
@@ -1,1 +1,17 @@
-"Luo labyrintti"
+
+"Luo labyrintin"
+
+from sklearn import neighbors
+
+
+size = 5
+graph = dict()
+
+for x in range (0, size):
+    for y in range (0, size):
+        node = (x,y)
+        graph[node] = list()
+
+
+print(graph)
+
