1c2ae96c2485a7b9f93dae365556f9b35315bb63
Janne Kärki <janne.karki@helsinki.fi>
2022-08-08 17:38:48 +0300

    Suunnanmuutokset oikealle ja vasemmalle, sekä metodi eteenpäin kulkemiselle lisätty ja kulkemista parannettu. Algoritmi saa nyt jonkin ratkaisun aikaiseksi

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -49,22 +49,21 @@ class WallFollower:
             cell = (cell[0], cell[1]-1)
             return True, cell
         return False, cell
+
+    def no_wall_in_front(self, cell, moving_direction):
+        air_directions = self.maze[cell]
+        if air_directions[moving_direction] == 1:  
+            return True
+        return False    
     
     def try_right(self, cell, moving_direction):
         air_directions = self.maze[cell]
-        if air_directions[moving_direction] == 1:
-            if moving_direction == north:
-                cell = (cell[0]-1, cell[1])
-            if moving_direction == east:
-                cell = (cell[0], cell[1]+1)
-            if moving_direction == south:
-                cell = (cell[0]+1, cell[1])
-            if moving_direction == west:
-                cell = (cell[0], cell[1]-1)
-            return True, cell
-        return False, cell
+        right = self.turn_where_is_right(moving_direction)
+        if air_directions[right] == 1:  
+            return True
+        return False
 
-    def where_is_right(self, moving_direction):
+    def turn_where_is_right(self, moving_direction):
         if moving_direction == north:
             return east 
         if moving_direction == east:
@@ -74,20 +73,51 @@ class WallFollower:
         if moving_direction == west:
             return north
 
+    def turn_where_is_left(self, moving_direction):
+        if moving_direction == north:
+            return west 
+        if moving_direction == east:
+            return north
+        if moving_direction == south:
+            return east
+        if moving_direction == west:
+            return south
+
+    def move_forward(self, cell, moving_direction):
+        if moving_direction == north:
+            cell = (cell[0]-1, cell[1])
+        if moving_direction == east:
+            cell = (cell[0], cell[1]+1)
+        if moving_direction == south:
+            cell = (cell[0]+1, cell[1])
+        if moving_direction == west:
+            cell = (cell[0], cell[1]-1)
+        return cell
+
     def solve_maze(self):
         start = 0,0
         cell = start
         moving_direction = east
-
-        while cell != (self.maze_size-1,self.maze_size-1):
+        while cell != ((self.maze_size-1,self.maze_size-1)):
             print(cell)
-
-            moved, cell = self.try_right(cell, moving_direction)
-            if moved:
-                moving_direction = self.where_is_right(moving_direction)
             
+            if self.try_right(cell, moving_direction):
+                moving_direction = self.turn_where_is_right(moving_direction)
+                cell = self.move_forward(cell, moving_direction)
+                print("turned and moved to right")
+                continue
 
+            if self.no_wall_in_front(cell, moving_direction):    
+                cell = self.move_forward(cell, moving_direction)
+                print("no wall in front")
+                continue
+            
+            else:
+                moving_direction = self.turn_where_is_left(moving_direction)
+                print("wall in front, turn left")
+                continue
 
+"""
             if moving_direction == south:
                 moved, cell = self.move_down(cell)
                 if not moved:
@@ -112,7 +142,7 @@ class WallFollower:
                 if not moved:
                     moving_direction = self.where_is_right(moving_direction)
                     continue
-        
+"""        
         
 
 
