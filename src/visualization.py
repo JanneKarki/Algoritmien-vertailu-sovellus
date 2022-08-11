36d968d036e95f53b1e2f4af60155609d1d39ef4
Janne Kärki <janne.karki@helsinki.fi>
2022-08-11 07:58:35 +0300

    sisään ja ulos reitit labyrinttiin

--- src/visualization.py
+++ src/visualization.py
@@ -10,14 +10,16 @@ class Visualization:
     def draw_maze_image(self, maze, maze_size):
         img = np.zeros((maze_size * (self.cell_thickness + self.wall_thickness) + self.wall_thickness,
                 maze_size * (self.cell_thickness + self.wall_thickness) + self.wall_thickness),dtype=np.uint8)
-
+        image_size = (maze_size * (self.cell_thickness + self.wall_thickness) + self.wall_thickness)
         for edge in maze:
+
             min_x = self.wall_thickness+min(edge[0][0],edge[1][0])*(self.cell_thickness + self.wall_thickness)
             max_x = self.wall_thickness+max(edge[0][0],edge[1][0])*(self.cell_thickness + self.wall_thickness)
             min_y = self.wall_thickness+min(edge[0][1],edge[1][1])*(self.cell_thickness + self.wall_thickness)
             max_y = self.wall_thickness+max(edge[0][1],edge[1][1])*(self.cell_thickness + self.wall_thickness)
             img[min_x:max_x+self.cell_thickness,min_y:max_y+self.cell_thickness] = 255
-
+            img[0:self.wall_thickness , self.wall_thickness :self.cell_thickness+self.wall_thickness] = 255
+            img[image_size-self.wall_thickness:image_size, image_size-(self.cell_thickness+self.wall_thickness):image_size-self.wall_thickness] = 255
         image = Image.fromarray(img)
         image.save("src/data/maze.png")
         return image
