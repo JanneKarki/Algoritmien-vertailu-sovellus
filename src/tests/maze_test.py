b87e37b9cbb14245eded6063f8bcbd071c4b89ec
Janne Kärki <janne.karki@helsinki.fi>
2022-08-11 22:12:57 +0300

    testit korjattu

--- src/tests/maze_test.py
+++ src/tests/maze_test.py
@@ -3,7 +3,6 @@ from maze import Maze
 
 class TestMaze(unittest.TestCase):
     def setUp(self):
-        print("Set up goes here")
         self.maze = Maze(5)
 
     def test_maze_size_is_correct(self):
@@ -11,26 +10,23 @@ class TestMaze(unittest.TestCase):
         self.assertEqual(size, 5)
 
     def test_maze_created_with_kruskal(self):
-        maze = self.maze.maze_by_kruskal()
+        maze = self.maze.solution
         empty_maze = []
         self.assertNotEqual(maze, empty_maze)
 
     def test_maze_turned_to_air_directed_set(self):
-        maze = self.maze.maze_by_kruskal()
-        air_directed_maze = self.maze.maze_in_air_directions(maze)
+        air_directed_maze = self.maze.air_directed_maze
+        print(air_directed_maze, "airdirectedmaze???????????????")
         empty_set = {}
         self.assertNotEqual(air_directed_maze, empty_set)
 
     def test_air_directed_set_length_correct(self):
-        maze = self.maze.maze_by_kruskal()
-        air_directed_maze = self.maze.maze_in_air_directions(maze)
-        length = len(air_directed_maze)
+        length = len(self.maze.air_directed_maze)
         self.assertEqual(length, 5*5)
 
     def test_air_directed_maze_set_has_starting_point(self):
-        maze = self.maze.maze_by_kruskal()
-        air_directed_maze = self.maze.maze_in_air_directions(maze)
+        maze = self.maze.air_directed_maze
         start = (0,0)
-        find = start in air_directed_maze
+        find = start in maze
         self.assertEqual(True, find)
 
