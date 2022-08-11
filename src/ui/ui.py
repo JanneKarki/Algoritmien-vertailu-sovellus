e06504d771bb909fe97d41eb8fb4984ee91722b6
Janne Kärki <janne.karki@helsinki.fi>
2022-08-11 19:56:59 +0300

    järkeistetty käyttöliittymän rakennetta

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -15,56 +15,64 @@ class UI:
         """
       
         self._root = root
-        self.class_maze = Maze(10)
         self._frame = ttk.Frame(master=self._root)
-        self.maze = self.class_maze.maze_by_kruskal()
-        self.maze_air_directed = self.class_maze.maze_in_air_directions(self.maze)
-        self.wall_follower = WallFollower(self.maze_air_directed)
-        self.image = visualization.draw_maze_image(self.maze, 10)
-        self.image = self.wall_follower.draw_solution()
-        self.photo_image = tk.PhotoImage(file="src/data/maze.png")
-        self.maze_label = ttk.Label(master=self._frame, image=self.photo_image)
-
+        self.class_maze = None
+        self.generated_maze = None
+        self.air_directed_maze = None  
+        self.maze_label = ttk.Label(master=self._frame, image=None)
 
     def start(self):
-        """Alustaa ja näyttää sovelluksen näkymän.
-        """
+        """Alustaa sovelluksen näkymän.
+        """     
 
         heading_label = ttk.Label(master=self._frame, text="Labyrintti")
         heading_label.grid(row=0, column=0)
-   
-        self.maze_label.grid(row=1, column=0 )
         solve_label = ttk.Label(master=self._frame, text="Ratkaise labyrintti:")
 
         s = ttk.Style()
         s.configure("TFrame", background="DarkSeaGreen1")
         s.configure("TLabel", background="DarkSeaGreen1")
-        self._frame.grid_columnconfigure(0, weight=1, minsize=150)
-        self._frame.pack()
         
         generate_maze_button = ttk.Button(
             master=self._frame,
             text="Luo uusi labyrintti",
-            command="")
+            command=self.handle_new_maze_click)
 
         wall_follower_button = ttk.Button(
             master=self._frame,
             text="Wall Follower",
-            command=self.load_photo)        
+            command=self.wall_follower)        
 
-
+        self.maze_label.grid(row=1, column=0 )
         generate_maze_button.grid(row=3, column=0, padx=5, pady=5)
         solve_label.grid(row=4, column=0)
         wall_follower_button.grid(row=5, column=0, padx=5, pady=5)
+
+        self._frame.grid_columnconfigure(0, weight=1, minsize=150)
+        self._frame.pack()
         
-      
 
-    def load_solved_maze(self):
-        solved_maze = tk.PhotoImage(file="src/data/solution.png")
-        print(solved_maze)
-        self.photo_variable.set(solved_maze)
+    def wall_follower(self):
+        wall_follower = WallFollower(self.air_directed_maze)
+        wall_follower.draw_solution()
+        self.show_solution()
 
-    def load_photo(self):
+    def show_solution(self):
         photo = tk.PhotoImage(file="src/data/solution.png")
         self.maze_label.configure(image=photo)
         self._root.mainloop()
+        
+    def handle_new_maze_click(self):
+        self.load_new_maze( 50)
+
+    def load_new_maze(self, maze_size):
+        self.class_maze = Maze(maze_size)
+        self.generated_maze = self.class_maze.solution
+        self.air_directed_maze = self.class_maze.air_directed_maze
+        visualization.draw_maze_image(self.generated_maze, maze_size)
+        photo = tk.PhotoImage(file="src/data/maze.png")
+        self.maze_label.configure(image=photo)
+        self._root.mainloop()
+
+  
+        
