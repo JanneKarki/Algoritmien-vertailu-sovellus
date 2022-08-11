726f1fb34ea3f9da50af9f4b001935ecafa25b71
Janne Kärki <janne.karki@helsinki.fi>
2022-08-11 20:03:08 +0300

    labyrintin ratkaisut luokan konstruktoriin

--- src/maze.py
+++ src/maze.py
@@ -9,6 +9,9 @@ class Maze:
         self.graph_edges = []
         self.disjoint_set = {}
         self.solution = []
+        self.air_directed_maze = {}
+
+        self.maze_by_kruskal()
 
     def maze_by_kruskal(self):
         for x in range (0, self.maze_size):
@@ -57,7 +60,7 @@ class Maze:
 
                 self.graph_edges.remove(rnd_edge)
         
-        return self.solution
+        return self.maze_in_air_directions(self.solution)
 
     def _find(self, element):
         if self.disjoint_set[element] == element:
@@ -76,37 +79,37 @@ class Maze:
         "N,E,S,W"
         for x in range(self.maze_size):
             for y in range(self.maze_size):
-                air_directions[(x,y)] = (0,0,0,0)
+                self.air_directed_maze[(x,y)] = (0,0,0,0)
 
         for edge in maze:
             x = (edge[1][0]-edge[0][0])
 
             if x == 1: #solusta pääsee alas
-                north = air_directions[edge[0]][0]
-                east = air_directions[edge[0]][1]
+                north = self.air_directed_maze[edge[0]][0]
+                east = self.air_directed_maze[edge[0]][1]
                 south = 1
-                west = air_directions[edge[0]][3]
-                air_directions[edge[0]] = (north,east,south,west) #solusta pääsee alas
+                west = self.air_directed_maze[edge[0]][3]
+                self.air_directed_maze[edge[0]] = (north,east,south,west) #solusta pääsee alas
                 
                 north = 1
-                east = air_directions[edge[1]][1]
-                south = air_directions[edge[1]][2]
-                west = air_directions[edge[1]][3]
-                air_directions[edge[1]] = (1,east,south,west) #solusta pääsee ylös
+                east = self.air_directed_maze[edge[1]][1]
+                south = self.air_directed_maze[edge[1]][2]
+                west = self.air_directed_maze[edge[1]][3]
+                self.air_directed_maze[edge[1]] = (1,east,south,west) #solusta pääsee ylös
 
             y = (edge[1][1]-edge[0][1])
             if y == 1: #solusta pääsee oikealle
-                north = air_directions[edge[0]][0]
+                north = self.air_directed_maze[edge[0]][0]
                 east = 1
-                south = air_directions[edge[0]][2]
-                west = air_directions[edge[0]][3]
+                south = self.air_directed_maze[edge[0]][2]
+                west = self.air_directed_maze[edge[0]][3]
 
-                air_directions[edge[0]] = (north,east,south,west) #solusta pääsee oikealle
+                self.air_directed_maze[edge[0]] = (north,east,south,west) #solusta pääsee oikealle
                 
-                north = air_directions[edge[1]][0]
-                east = air_directions[edge[1]][1]
-                south = air_directions[edge[1]][2]
+                north = self.air_directed_maze[edge[1]][0]
+                east = self.air_directed_maze[edge[1]][1]
+                south = self.air_directed_maze[edge[1]][2]
                 west = 1
-                air_directions[edge[1]] = (north,east,south,west) #solusta pääsee vasemmalle
+                self.air_directed_maze[edge[1]] = (north,east,south,west) #solusta pääsee vasemmalle
 
-        return air_directions    
+        return self.air_directed_maze    
