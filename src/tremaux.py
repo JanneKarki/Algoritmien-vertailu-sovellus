8cf29612c9c66c7f05524e1a0e49982e043cbfac
Janne Kärki <janne.karki@helsinki.fi>
2022-08-13 08:49:26 +0300

    Tremaux luokka lisätty jossa alustava liikkuminen

---
+++ src/tremaux.py
@@ -0,0 +1,63 @@
+import math
+
+north = 0
+east = 1
+south = 2
+west = 3
+
+class Tremaux:
+    def __init__(self, maze):
+        self.maze = maze
+        self.solution = []
+        self.maze_size = math.sqrt(len(self.maze))
+        #self.wall_thickness = 6
+        #self.cell_thickness = 20
+        #self.total_cell_thickness = 26
+        #self.cell_center = 13
+        #self.elapsed_time = None
+
+    def no_wall_in_front(self, cell, moving_direction):
+        air_directions = self.maze[cell]
+        if air_directions[moving_direction] == 1:  
+            return True
+        return False    
+    
+    def try_right(self, cell, moving_direction):
+        air_directions = self.maze[cell]
+        right = self.turn_where_is_right(moving_direction)
+        if air_directions[right] == 1:  
+            return True
+        return False
+
+    def turn_where_is_right(self, moving_direction):
+        if moving_direction == north:
+            return east 
+        if moving_direction == east:
+            return south
+        if moving_direction == south:
+            return west
+        if moving_direction == west:
+            return north
+
+    def turn_where_is_left(self, moving_direction):
+        if moving_direction == north:
+            return west 
+        if moving_direction == east:
+            return north
+        if moving_direction == south:
+            return east
+        if moving_direction == west:
+            return south
+
+    def move_forward(self, cell, moving_direction):
+        if moving_direction == north:
+            cell = (cell[0]-1, cell[1])
+        if moving_direction == east:
+            cell = (cell[0], cell[1]+1)
+        if moving_direction == south:
+            cell = (cell[0]+1, cell[1])
+        if moving_direction == west:
+            cell = (cell[0], cell[1]-1)
+        return cell
+
+   
