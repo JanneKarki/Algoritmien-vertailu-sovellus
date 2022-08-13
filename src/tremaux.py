245f57bb6a0659b4115aeae5d4b784153c57d8b2
Janne Kärki <janne.karki@helsinki.fi>
2022-08-13 22:19:33 +0300

    Käännökset ympäri ja vasemmalle, sekä labyrintin ratkaisu metodi

--- src/tremaux.py
+++ src/tremaux.py
@@ -1,4 +1,6 @@
 import math
+from maze import Maze
+import random
 
 north = 0
 east = 1
@@ -25,6 +27,23 @@ class Tremaux:
     def try_right(self, cell, moving_direction):
         air_directions = self.maze[cell]
         right = self.turn_where_is_right(moving_direction)
+        if air_directions[right] == 1:  
+            return True
+        return False
+
+    def turn_around(self, moving_direction):
+        if moving_direction == north:
+            return south
+        if moving_direction == south:
+            return north
+        if moving_direction == east:
+            return west
+        if moving_direction == west:
+            return east
+
+    def try_left(self, cell, moving_direction):
+        air_directions = self.maze[cell]
+        right = self.turn_where_is_left(moving_direction)
         if air_directions[right] == 1:  
             return True
         return False
@@ -60,4 +79,52 @@ class Tremaux:
             cell = (cell[0], cell[1]-1)
         return cell
 
-   
+    def solve_maze(self):
+        start = 0,0
+        cell = start
+        moving_direction = east
+        visited = {}
+
+        while cell != ((self.maze_size-1,self.maze_size-1)):
+            available_directions = []
+
+           
+            if self.try_right(cell, moving_direction) and cell not in visited:
+                available_directions.append(self.turn_where_is_right(moving_direction))
+            if self.no_wall_in_front(cell, moving_direction) and cell not in visited:
+                available_directions.append(moving_direction)
+            if self.try_left(cell, moving_direction) and cell not in visited:
+                available_directions.append(self.turn_where_is_left(moving_direction))
+
+            if not available_directions:
+                if self.try_right(cell, moving_direction) and cell in visited:
+                    if visited[cell] == 1:
+                        available_directions.append(self.turn_where_is_right(moving_direction))
+                if self.no_wall_in_front(cell, moving_direction) and cell in visited:
+                    if visited[cell] == 1:
+                        available_directions.append(moving_direction)
+                if self.try_left(cell,moving_direction) and cell in visited:
+                    available_directions.append(self.turn_where_is_left(moving_direction))
+
+            if not available_directions:
+                moving_direction = self.turn_around(moving_direction)
+                
+
+
+            #else:
+            print(available_directions)
+            moving_direction = random.choice(available_directions)
+            if cell not in visited:
+                visited[cell] = 1
+            else:
+                visited[cell] = 2
+
+            cell = self.move_forward(cell, moving_direction)
+            self.solution.append(cell)
+            print(self.solution)
+
+if __name__ == '__main__':
+    maze_class = Maze(6)
+    maze = maze_class.air_directed_maze
+    tremaux = Tremaux(maze)
+    tremaux.solve_maze()
