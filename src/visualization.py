ddc9230ea5dc0cc9d5280a2b8d667e0bfc8022b1
Janne Kärki <janne.karki@helsinki.fi>
2022-08-14 10:17:23 +0300

    Docstringit lisätty

--- src/visualization.py
+++ src/visualization.py
@@ -2,13 +2,21 @@ from PIL import Image
 import numpy as np
 import math
 class Visualization:
+    """Luokka joka vastaa labyrintin piirtämisestä kuvaksi"""
 
     def __init__(self):
+        """Luokan konstruktori, joka määritää labyrintin solun ja seinän paksuuden.
+        """
         self.cell_thickness = 20
         self.wall_thickness = 6
 
     def draw_maze_image(self, maze, maze_size):
-        print(maze_size)
+        """Piirtää tallentaa labyrintin kuvan
+
+        Args:
+            maze (list): Piirrettävä labyrintti.
+            maze_size (int): Labyrintin koko.
+        """
         self.adjust_image_size(maze_size)
         img = np.zeros((maze_size * (self.cell_thickness + self.wall_thickness) + self.wall_thickness,
                 maze_size * (self.cell_thickness + self.wall_thickness) + self.wall_thickness),dtype=np.uint8)
@@ -24,11 +32,16 @@ class Visualization:
             img[image_size-self.wall_thickness:image_size, image_size-(self.cell_thickness+self.wall_thickness):image_size-self.wall_thickness] = 255
         image = Image.fromarray(img)
         image.save("src/data/maze.png")
-        return image
+
 
     def adjust_image_size(self, maze_size):
+        """Säätäää solun ja seinämän paksuutta niin että kuvan ulkomitat
+           pysyvät samankokoisina eri kokoisilla labyrinteillä.
+
+        Args:
+            maze_size (int): Labyrintin koko.
+        """
         if maze_size > 10:
-            print("Big")
             self.cell_thickness = int((10/maze_size)*20)
             self.wall_thickness = int((10/maze_size)*6)
             if self.wall_thickness < 1:
