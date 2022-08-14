5b486da408bf850b3e68a088892fc43c7de709ad
Janne Kärki <janne.karki@helsinki.fi>
2022-08-14 09:58:36 +0300

    Docstringit lisätty

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -8,7 +8,18 @@ south = 2
 west = 3
 
 class WallFollower:
+    """Luokka joka vastaa labyrintin ratkaisusta Wall-Follower algoritmilla,
+    joka oikean puoleista seinää seuraamalla, löytää ratkaisun.
+    """
     def __init__(self, maze):
+        """Luokan konstruktorin joka luo Wall-Follower-Algoritmista vastaavan
+           palvelun.
+
+        Args:
+            maze (dict): Labyrintti sanakirjassa, avaimena solut jotka sisältävät
+                        tuplen (0,0,0,0) (north,east,south,west) nolla=seinä,
+                        1=pääsy.
+        """
         self.maze = maze
         self.solution = []
         self.maze_size = math.sqrt(len(self.maze))
@@ -20,12 +31,30 @@ class WallFollower:
         
 
     def no_wall_in_front(self, cell, moving_direction):
+        """Tarkistaa onko edessä seinää. Jos on, palauttaa False, muuten True.
+
+        Args:
+            cell (tuple): Solu jossa algoritmi on etenemässä.
+            moving_direction (int): Suunta johon ollaan kulkemassa.
+
+        Returns:
+            boolean: Palauttaa True, jos edessä ei ole seinää, muuten False.
+        """
         air_directions = self.maze[cell]
         if air_directions[moving_direction] == 1:  
             return True
         return False    
     
     def try_right(self, cell, moving_direction):
+        """Tarkistaa voko solusta kääntyä oikealle.
+
+        Args:
+            cell (tuple): Solu jossa algorimti on etenemässä.
+            moving_direction (int): Suunta, johon ollaan kulkemassa.
+
+        Returns:
+            Boolean: Palauttaa True, jos oikealle voi kulkea, muuten palauttaa False.
+        """
         air_directions = self.maze[cell]
         right = self.turn_where_is_right(moving_direction)
         if air_directions[right] == 1:  
@@ -33,6 +62,14 @@ class WallFollower:
         return False
 
     def turn_where_is_right(self, moving_direction):
+        """Palauttaa ilmansuunnan, joka on kulkusuunnasta oikealla.
+
+        Args:
+            moving_direction (int): Suunta, johon ollaan kulkemassa.
+
+        Returns:
+            int: Palauttaa kulkusuunnasta oikealla olevan ilmansuunnan. 
+        """
         if moving_direction == north:
             return east 
         if moving_direction == east:
@@ -43,6 +80,14 @@ class WallFollower:
             return north
 
     def turn_where_is_left(self, moving_direction):
+        """Palauttaa ilmansuunnan, joka on kulkusuunnasta vasemmalla.
+
+        Args:
+            moving_direction (int): Suunta, johon ollaan kulkemassa.
+
+        Returns:
+            int: Palauttaa kulkusuunnasta vasemmalla olevan ilmansuunnan.
+        """
         if moving_direction == north:
             return west 
         if moving_direction == east:
@@ -53,6 +98,15 @@ class WallFollower:
             return south
 
     def move_forward(self, cell, moving_direction):
+        """Siirtää algoritmin seuraavaan soluun.
+
+        Args:
+            cell (tuple): Solu, jossa nyt ollaan.
+            moving_direction (int): Suunta johon ollaan etenemässä.
+
+        Returns:
+            tuple: Palauttaa solun johon siirryttiin.
+        """
         if moving_direction == north:
             cell = (cell[0]-1, cell[1])
         if moving_direction == east:
@@ -64,37 +118,39 @@ class WallFollower:
         return cell
 
     def solve_maze(self):
+        """Ratkaisee labyrintin, alkupisteenä vasen yläkulma ja loppupisteenä
+           oikea alakulma, ja mittaa siihen kuluneen ajan.
+        """
         start = 0,0
         cell = start
         moving_direction = east
         start_time = time.time()
-        while cell != ((self.maze_size-1,self.maze_size-1)):
+        end = (self.maze_size-1,self.maze_size-1)
+        while cell != (end):
             
             if self.try_right(cell, moving_direction):
                 moving_direction = self.turn_where_is_right(moving_direction)
                 cell = self.move_forward(cell, moving_direction)
                 self.solution.append(cell)
-                #print("turned and moved to right")
                 continue
 
             if self.no_wall_in_front(cell, moving_direction):    
                 cell = self.move_forward(cell, moving_direction)
-                #print("no wall in front")
                 self.solution.append(cell)
                 continue
             
             else:
                 moving_direction = self.turn_where_is_left(moving_direction)
-                #print("wall in front, turn left")
                 continue
+
         end_time = time.time()
         last_step = (self.maze_size-1,self.maze_size-1)
         self.solution.append(last_step)
-        print(len(self.solution))
-
         self.count_time(start_time, end_time)
 
     def draw_solution(self):
+        """Piirtää ratkaistun reitin labyrintin kuvaan.
+        """
         self.adjust_solution_size()
         self.solve_maze()
         with Image.open("src/data/maze.png") as im:
@@ -116,6 +172,8 @@ class WallFollower:
             im.save("src/data/solution.png")
 
     def adjust_solution_size(self):
+        """Säätää piirrettävän ratkaisun kokoa labyrintin kokoa vastaavaksi.
+        """
         if self.maze_size > 10:
             self.cell_thickness = int((10/self.maze_size)*20)
             self.wall_thickness = int((10/self.maze_size)*6)
@@ -128,6 +186,11 @@ class WallFollower:
             self.wall_thickness = 6
 
     def count_time(self, start_time, end_time):
+        """Laskee labyrintin ratkaisuun kuluneen ajan.
+
+        Args:
+            start_time (float): Ratkaisun aloitusaika.
+            end_time (float): Ratkaisun valmistumisaika.
+        """
         elapsed_time = "{0:.5f}".format(end_time-start_time)
         self.elapsed_time = elapsed_time
-        print(elapsed_time)
