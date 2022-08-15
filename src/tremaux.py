162dec57fad76453a696c5e7dfed52cad176d801
Janne Kärki <janne.karki@helsinki.fi>
2022-08-15 15:24:31 +0300

    vierailtujen solujen listat konstruktoriin

--- src/tremaux.py
+++ src/tremaux.py
@@ -19,6 +19,8 @@ class Tremaux:
         self.total_cell_thickness = 26
         self.cell_center = 13
         self.elapsed_time = None
+        self.visited = []
+        self.visited_twice = []
 
     def solve_maze(self):
         start = 0,0
@@ -32,37 +34,29 @@ class Tremaux:
             once_visited_directions = []
 
             print(cell)
-            visited.append(cell)
+            self.visited.append(cell)
             left_cell = self.explore.turn_where_is_left(moving_direction)
             right_cell = self.explore.turn_where_is_right(moving_direction)
             front_cell = moving_direction
 
-            # tarkistaa onko kulkemattomia suuntia
-            if self.explore.try_right(cell, moving_direction) and self.explore.move_forward(cell, right_cell) not in visited:
-                unvisited_directions.append(right_cell)
-            if self.explore.no_wall_in_front(cell, moving_direction) and self.explore.move_forward(cell, front_cell) not in visited:
-                unvisited_directions.append(moving_direction)
-            if self.explore.try_left(cell, moving_direction) and self.explore.move_forward(cell, left_cell) not in visited:
-                unvisited_directions.append(left_cell)
+        
 
-            # jos kulkemattomia suuntia käänny random ja kulje askel eteenpäin
+            unvisited_directions = self.not_visited_directions(cell, moving_direction)
             if unvisited_directions:
-                print(unvisited_directions, "unvisited directions")
-                
-                moving_direction = random.choice(unvisited_directions)
-                print(moving_direction, " moving direction")  
+                moving_direction = random.choice(unvisited_directions)  
                 cell = self.explore.move_forward(cell, moving_direction)
                 self.solution.append(cell)
                 continue
+
             #jos ei ole kulkemattomia suuntia tarkista onko kerran kuljettuja suuntia
             if not unvisited_directions:
                 print("not unvisited directions")
-                if self.explore.try_right(cell, moving_direction) and self.explore.move_forward(cell, right_cell) in visited and self.explore.move_forward(cell, right_cell) not in visited_twice:
+                if self.explore.try_right(cell, moving_direction) and self.explore.move_forward(cell, right_cell) in self.visited and self.explore.move_forward(cell, right_cell) not in self.visited_twice:
                     once_visited_directions.append(right_cell)
-                if self.explore.no_wall_in_front(cell, moving_direction) and self.explore.move_forward(cell, front_cell) in visited and self.explore.move_forward(cell, front_cell) not in visited_twice:
+                if self.explore.no_wall_in_front(cell, moving_direction) and self.explore.move_forward(cell, front_cell) in self.visited and self.explore.move_forward(cell, front_cell) not in self.visited_twice:
                     once_visited_directions.append(moving_direction)
                     print("eteenpäin pääsee takaisin ")
-                if self.explore.try_left(cell,moving_direction) and self.explore.move_forward(cell, left_cell) in visited and self.explore.move_forward(cell, left_cell) not in visited_twice:
+                if self.explore.try_left(cell,moving_direction) and self.explore.move_forward(cell, left_cell) in self.visited and self.explore.move_forward(cell, left_cell) not in self.visited_twice:
                     once_visited_directions.append(left_cell)
 
                 print(once_visited_directions)
@@ -74,7 +68,7 @@ class Tremaux:
                 else:
                     if len(once_visited_directions) == 1:
                         print("vain yksi suunta jo kuljettu suunta")
-                        visited_twice.append(cell)
+                        self.visited_twice.append(cell)
                         moving_direction = once_visited_directions[0]
                         cell = self.explore.move_forward(cell, moving_direction)
                         self.solution.append(cell)
@@ -85,8 +79,26 @@ class Tremaux:
                         self.solution.append(cell)
                         continue
 
-        print(visited_twice, "visited twice")
+        print(self.visited_twice, "visited twice")
         print(self.solution)
+
+    def not_visited_directions(self, cell, moving_direction):
+        unvisited_directions = []
+
+        left_cell = self.explore.turn_where_is_left(moving_direction)
+        right_cell = self.explore.turn_where_is_right(moving_direction)
+        front_cell = moving_direction
+
+        if self.explore.try_right(cell, moving_direction) and self.explore.move_forward(cell, right_cell) not in self.visited:
+            unvisited_directions.append(right_cell)
+        if self.explore.no_wall_in_front(cell, moving_direction) and self.explore.move_forward(cell, front_cell) not in self.visited:
+            unvisited_directions.append(moving_direction)
+        if self.explore.try_left(cell, moving_direction) and self.explore.move_forward(cell, left_cell) not in self.visited:
+            unvisited_directions.append(left_cell)
+        
+        return unvisited_directions
+        
+
 
     def draw_solution(self):
             """Piirtää ratkaistun reitin labyrintin kuvaan.
