bf62df0c5521942f55c324b7d4ef7912ce5ea48a
Janne Kärki <janne.karki@helsinki.fi>
2022-08-31 10:55:17 +0300

    Algortimien testausta lisätty

--- src/tests/tremaux_test.py
+++ src/tests/tremaux_test.py
@@ -5,18 +5,33 @@ from algorithms.tremaux import Tremaux
 
 class TestTremaux(unittest.TestCase):
     def setUp(self):
-        self.maze = Maze(5)
-        self.tremaux = Tremaux(self.maze.air_directed_maze)
-        self.my_maze = {(0, 0): (0, 1, 0, 0),
-                        (0, 1): (0, 0, 1, 1),
-                        (0, 2): (0, 0, 1, 0),
-                        (1, 0): (0, 1, 1, 0),
-                        (1, 1): (1, 1, 1, 1),
-                        (1, 2): (1, 0, 1, 1),
-                        (2, 0): (1, 0, 0, 0),
-                        (2, 1): (1, 0, 0, 0),
-                        (2, 2): (1, 0, 0, 0)}
+        self.my_maze = {(0, 0): (0, 1, 1, 0),(0, 1): (0, 0, 0, 1),(0, 2): (0, 1, 1, 0),(0, 3): (0, 0, 1, 1),
+                        (0, 4): (0, 0, 1, 0),(0, 5): (0, 1, 1, 0),(0, 6): (0, 0, 0, 1),(1, 0): (1, 1, 0, 0),
+                        (1, 1): (0, 1, 1, 1),(1, 2): (1, 0, 0, 1),(1, 3): (1, 0, 0, 0),(1, 4): (1, 1, 0, 0),
+                        (1, 5): (1, 0, 1, 1),(1, 6): (0, 0, 1, 0),(2, 0): (0, 0, 1, 0),(2, 1): (1, 1, 0, 0),
+                        (2, 2): (0, 1, 0, 1), (2, 3): (0, 1, 0, 1),(2, 4): (0, 0, 1, 1), (2, 5): (1, 0, 1, 0),
+                        (2, 6): (1, 0, 1, 0), (3, 0): (1, 0, 1, 0),(3, 1): (0, 0, 1, 0), (3, 2): (0, 1, 0, 0),
+                        (3, 3): (0, 0, 1, 1), (3, 4): (1, 1, 1, 0),(3, 5): (1, 1, 0, 1), (3, 6): (1, 0, 0, 1),
+                        (4, 0): (1, 1, 1, 0), (4, 1): (1, 0, 0, 1),(4, 2): (0, 1, 0, 0), (4, 3): (1, 1, 1, 1),
+                        (4, 4): (1, 0, 0, 1), (4, 5): (0, 1, 1, 0),(4, 6): (0, 0, 0, 1), (5, 0): (1, 1, 0, 0),
+                        (5, 1): (0, 1, 1, 1), (5, 2): (0, 1, 0, 1),(5, 3): (1, 0, 1, 1), (5, 4): (0, 1, 1, 0),
+                        (5, 5): (1, 1, 0, 1), (5, 6): (0, 0, 1, 1),(6, 0): (0, 1, 0, 0), (6, 1): (1, 0, 0, 1),
+                        (6, 2): (0, 1, 0, 0), (6, 3): (1, 1, 0, 1),(6, 4): (1, 1, 0, 1), (6, 5): (0, 0, 0, 1),
+                        (6, 6): (1, 0, 0, 0)}
+        self.tremaux = Tremaux(self.my_maze)
+        self.maze_size = self.tremaux.maze_size
 
-    def test_maze_size_is_correct(self):
-        maze_size = self.tremaux.maze_size
-        self.assertEqual(maze_size, 5)
+    def test_maze_size_is_correct(self): 
+        self.assertEqual(self.maze_size, 7)
+
+    def test_maze_is_solved(self):
+        solved = self.tremaux.solution != []
+        self.assertEqual(True, solved)
+
+    def test_tremaux_ends_in_right_down_corner(self):
+        end = self.tremaux.solution[len(self.tremaux.solution)-1]
+        self.assertEqual(end, ((self.maze_size)-1,(self.maze_size)-1))
+    
+    def test_elapsed_time_is_calculated(self):
+        measured_time = float(self.tremaux.elapsed_time) > 0
+        self.assertEqual(True, measured_time)
--- src/tests/wall_follower_test.py
+++ src/tests/wall_follower_test.py
@@ -9,9 +9,20 @@ class TestWallFollower(unittest.TestCase):
         self.kruskal_maze = self.class_maze.solution
         self.air_directed_maze = self.class_maze.air_directed_maze
         self.wall_follower = WallFollower(self.air_directed_maze)
+        self.solution = self.wall_follower.solution
+        self.maze_size =self.wall_follower.maze_size
 
     def test_maze_is_solved(self):
-        self.wall_follower.solve_maze()
-        solution = self.wall_follower.solution
-        empty = []
-        self.assertNotEqual(solution, empty)
+        solved = self.wall_follower.solution != []
+        self.assertEqual(True, solved)
+
+    def test_maze_size_is_correct(self): 
+        self.assertEqual(self.maze_size, 5)
+
+    def test_wall_follower_ends_in_down_right_corner(self):
+        end = self.wall_follower.solution[len(self.wall_follower.solution)-1]
+        self.assertEqual(end, ((self.maze_size)-1,(self.maze_size)-1))
+    
+    def test_elapsed_time_is_calculated(self):
+        measured_time = float(self.wall_follower.elapsed_time) > 0
+        self.assertEqual(True, measured_time)
